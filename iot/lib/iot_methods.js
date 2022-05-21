14f52997f86e0a6cf98596f7ab25914e8ae63253
Antonio Lain <antlai@cafjs.com>
2022-05-20 20:02:52 -0700

    Implement CA and IoT methods
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- iot/lib/iot_methods.js
+++ iot/lib/iot_methods.js
@@ -2,172 +2,48 @@
 
 const myUtils = require('caf_iot').caf_components.myUtils;
 
-
-const cleanupDeviceInfo = function(devices) {
-    const result = {};
-    Object.keys(devices).forEach((x) => {
-        result[x] = {
-            uuid: devices[x].uuid,
-            advertisement: myUtils.deepClone(devices[x].advertisement)
-        };
-    });
-    return result;
-};
+const COLOR_NONE = 255; // LED off
 
 exports.methods = {
     async __iot_setup__() {
-        // Example of how to store device state in the cloud, i.e.,
-        // the value of `index` from last run downloaded from the cloud.
-        const lastIndex = this.toCloud.get('index');
-        this.state.index = (lastIndex ? lastIndex : 0);
-
-        this.state.index = 0;
-        this.scratch.devices = {};
-        this.state.selectedDevice = null;
-        this.state.devicesInfo = {};
-        this.state.sensorValue = null;
+        this.state.isConnected = false;
+        this.state.colors = Array.from({length: 9}, () => COLOR_NONE);
 
         return [];
     },
 
     async __iot_loop__() {
-        this.$.log && this.$.log.debug(
-            'Time offset ' +
-                (this.$.cloud.cli && this.$.cloud.cli.getEstimatedTimeOffset())
-        );
-
-        this.toCloud.set('index', this.state.index);
-        this.state.index = this.state.index + 1;
-
-        const now = (new Date()).getTime();
-        this.$.log && this.$.log.debug(now + ' loop: ' + this.state.index);
-
-        if (!myUtils.deepEqual(this.toCloud.get('devices'),
-                               this.state.devicesInfo)) {
-            this.toCloud.set('devices', this.state.devicesInfo);
-        }
-        if (this.toCloud.get('sensorValue') !== this.state.sensorValue) {
-            this.toCloud.set('sensorValue', this.state.sensorValue);
+        if (this.toCloud.get('isConnected') !== this.state.isConnected) {
+            this.toCloud.set('isConnected', this.state.isConnected);
         }
 
-        return [];
-    },
-
-    async findDevices(config) {
-        const now = (new Date()).getTime();
-        this.$.log && this.$.log.debug(now + ' findDevices() config:' +
-                                       JSON.stringify(config));
-        this.state.config = config;
-
-        // forget old devices
-        this.scratch.devices = {};
-        this.state.devicesInfo = {};
-
-       if (typeof window !== 'undefined') {
-            // Wait for user click
-            await this.$.gatt.findServicesWeb(
-                config.service, '__iot_foundDevice__', 'confirmScan',
-                'afterConfirmScan'
-            );
-        } else {
-            this.$.gatt.findServices(config.service, '__iot_foundDevice__');
-        }
-        return [];
-    },
-
-    async __iot_foundDevice__(serviceId, device) {
-        if (serviceId.toLowerCase() ===
-            this.state.config.service.toLowerCase()) {
-            this.scratch.devices[device.uuid] = device;
-            this.state.devicesInfo = cleanupDeviceInfo(this.scratch.devices);
-        } else {
-            this.$.log && this.$.log.debug('Ignoring device with serviceID: ' +
-                                           serviceId + ' as opposed to ' +
-                                           this.state.config.service);
-        }
-        return [];
-    },
-
-    async connect(deviceId) {
-        this.$.log && this.$.log.debug('Selected device ' + deviceId);
-        if (this.state.selectedDevice) {
-            // Only one connected device
-            await this.disconnect();
-        }
-        this.state.selectedDevice = deviceId;
-
-        if (this.scratch.devices[deviceId]) {
-            try {
-                const {characteristics} =
-                          await this.$.gatt.findCharacteristics(
-                              this.state.config.service,
-                              this.scratch.devices[deviceId],
-                              [this.state.config.blink,
-                               this.state.config.notification]
-                          );
-                [this.scratch.blinkCharact, this.scratch.notificationCharact] =
-                    characteristics;
-                await this.$.gatt.subscribe(this.scratch.notificationCharact,
-                                            '__iot_subscribe__');
-                return [];
-            } catch (err) {
-                return [err];
+        if (this.state.isConnected) {
+            if (!myUtils.deepEqual(this.fromCloud.get('colors'),
+                                   this.state.colors)) {
+                this.state.colors = this.fromCloud.get('colors');
+                this.setMatrix(this.state.colors);
             }
-        } else {
-            this.$.log && this.$.log.debug('select: Ignoring unknown device ' +
-                                           deviceId);
-            return [];
         }
-    },
 
-    async __iot_subscribe__(charact, value) {
-        value = parseInt(value.toString('hex'), 16);
-        this.$.log && this.$.log.debug('Notification: got ' + value);
-        this.state.sensorValue = value;
         return [];
     },
 
-    async blink() {
-        if (this.scratch.blinkCharact) {
-            const buf = Buffer.from('on');
-            this.$.log && this.$.log.debug('Blinking');
-            await this.$.gatt.write(this.scratch.blinkCharact, buf);
-        }
-        return [];
-    },
-
-    async stop() {
-        if (this.scratch.blinkCharact) {
-            const buf = Buffer.from('off');
-            this.$.log && this.$.log.debug('Blinking Off');
-            await this.$.gatt.write(this.scratch.blinkCharact, buf);
-        }
+    async connect(deviceTypes) {
+        await this.$.lego.connect(deviceTypes);
+        this.state.isConnected = true;
         return [];
     },
 
     async disconnect() {
-        this.$.log && this.$.log.debug('Calling disconnect()');
-        if (this.scratch.notificationCharact) {
-            await this.$.gatt.unsubscribe(this.scratch.notificationCharact);
-        }
-        const device = this.state.selectedDevice &&
-                this.scratch.devices[this.state.selectedDevice];
-        if (device) {
-            this.$.log && this.$.log.debug('Disconnect device ' +
-                                           this.state.selectedDevice);
-            await this.$.gatt.disconnect(device);
-        }
-        this.state.selectedDevice = null;
+        await this.$.lego.disconnect();
+        this.state.isConnected = false;
         return [];
     },
 
-    async reset() {
-        this.$.log && this.$.log.debug('Calling reset()');
-        await this.disconnect();
-        await this.$.gatt.reset();
-        this.scratch.devices = {};
-        this.state.devicesInfo = {};
-        this.state.sensorValue = null;
+    async setMatrix(colors) {
+        await this.$.lego.callMethod(
+            'TECHNIC_3X3_COLOR_LIGHT_MATRIX', 'setMatrix', colors
+        );
         return [];
     }
 };
